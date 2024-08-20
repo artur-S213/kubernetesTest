f0f8e26f2c2d8cea56db53b3a98687efbfddf046
artur.scheling <artur.scheling@ohb-ds.de>
2024-08-20 15:44:55 +0200

    another try

--- login.sh
+++ login.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 echo "OkONTCGFX/UvwVrr2d7Am+PjEEWJZG24vuDsw8UmDH+ACRA5TF3w" | docker login artursreg.azurecr.io -u artursreg --password-stdin
+
+
+az login -u ${{ secrets.AZURE_LOGIN_USERNAME }} -p ${{ secrets.AZURE_LOGIN_PASSWORD }} --tenant ${{ secrets.AZURE_TENANT_ID }}
