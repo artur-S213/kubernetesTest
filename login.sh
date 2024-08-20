a9d4b89c4f3a139c7513b740f19263019a739fec
artur.scheling <artur.scheling@ohb-ds.de>
2024-08-20 15:52:52 +0200

    deployment without an substription setting

--- login.sh
+++ login.sh
@@ -6,3 +6,7 @@ echo "OkONTCGFX/UvwVrr2d7Am+PjEEWJZG24vuDsw8UmDH+ACRA5TF3w" | docker login artur
 az login -u ${{ secrets.AZURE_LOGIN_USERNAME }} -p ${{ secrets.AZURE_LOGIN_PASSWORD }} --tenant ${{ secrets.AZURE_TENANT_ID }}
 
 az login --service-principal -u ${{ secrets.AZURE_LOGIN_USERNAME }} -p ${{ secrets.AZURE_LOGIN_PASSWORD }} --tenant ${{ secrets.AZURE_TENANT_ID }}
+
+- name: Log in to Azure
+      run: |
+        az account set --subscription ${{ secrets.AZURE_SUBSCRIPTION_ID }}
