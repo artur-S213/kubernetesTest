d0ea55e576fc67d4ff94a8552ae63a5a4159a4f7
artur.scheling <artur.scheling@ohb-ds.de>
2024-08-20 15:47:22 +0200

    the other try

--- login.sh
+++ login.sh
@@ -4,3 +4,5 @@ echo "OkONTCGFX/UvwVrr2d7Am+PjEEWJZG24vuDsw8UmDH+ACRA5TF3w" | docker login artur
 
 
 az login -u ${{ secrets.AZURE_LOGIN_USERNAME }} -p ${{ secrets.AZURE_LOGIN_PASSWORD }} --tenant ${{ secrets.AZURE_TENANT_ID }}
+
+az login --service-principal -u ${{ secrets.AZURE_LOGIN_USERNAME }} -p ${{ secrets.AZURE_LOGIN_PASSWORD }} --tenant ${{ secrets.AZURE_TENANT_ID }}
